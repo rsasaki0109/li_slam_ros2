43ea2d8d670adedc63508a65eb5c3b254cd81fc9
ryohei sasaki <rsasaki0109@gmail.com>
2020-07-20 00:00:00 +0900

    Add a comparison and make the communication intra-process

--- scanmatcher/src/imu_preintegration.cpp
+++ scanmatcher/src/imu_preintegration.cpp
@@ -510,6 +510,7 @@ int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
     rclcpp::NodeOptions options;
+    options.use_intra_process_comms(true);
     rclcpp::executors::SingleThreadedExecutor exec;
     
     auto imu_pre = std::make_shared<IMUPreintegration>(options);
